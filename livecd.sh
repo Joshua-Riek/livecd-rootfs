97112cf5a4361d9202fe7668f02962b61f4c5add
Michael Casadevall <mcasadevall@ubuntu.com>
2009-08-04 10:52:12 -0400

    Move armel boards to subarchitecture and add dove armel subarchitecture

--- livecd.sh
+++ livecd.sh
@@ -326,7 +326,12 @@ link_in_boot = $link_in_boot
 	hppa)		LIST="$LIST linux-hppa32 linux-hppa64";;
 	powerpc)	LIST="$LIST linux-powerpc linux-powerpc64-smp";;
 	sparc*)		LIST="$LIST linux-sparc64";;
-	armel)		LIST="$LIST linux-imx51";;
+	armel)	
+			case "$SUBARCH" in 
+				imx51)	LIST="$LIST linux-imx51";;
+				dove)	LIST="$LIST linux-dove";;
+				*)	echo "Unknown armel subarch: no kernel." exit 1;;
+			esac;;
 	*)		echo "Unknown architecture: no kernel."; exit 1;;
     esac
 
