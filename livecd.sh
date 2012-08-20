2912599b8a2659e8cd680e770fb6b3712780bbb9
Stéphane Graber <stgraber@ubuntu.com>
2012-08-20 19:36:55 -0400

    Switch LTSP images to compressed squashfs to match new ltsp-update-image default

--- livecd.sh
+++ livecd.sh
@@ -813,7 +813,7 @@ Pin-Priority: 550
                 checkpoint "Building LTSP chroot"
                 ltsp-build-client --base $(pwd) --mirror $MIRROR --arch $TARGETARCH --dist $STE --chroot ltsp-live --late-packages ldm-edubuntu-theme,plymouth-theme-edubuntu --purge-chroot --skipimage
                 mkdir -p $(pwd)/images
-                mksquashfs $(pwd)/ltsp-live $(pwd)/images/ltsp-live.img -noF -noD -noI -no-exports -e cdrom
+                mksquashfs $(pwd)/ltsp-live $(pwd)/images/ltsp-live.img -e cdrom
                 rm -Rf $(pwd)/ltsp-live
                 if [ -f $(pwd)/images/ltsp-live.img ]; then
                     mv $(pwd)/images/ltsp-live.img livecd.$FS-ltsp.squashfs
