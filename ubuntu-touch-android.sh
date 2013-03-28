0330c329d29c020790f34cb3c0743b37dc43fc83
Oliver Grawert <ogra@ubuntu.com>
2013-03-28 16:32:16 +0100

    add android image cross building for Ubuntu Touch

---
+++ ubuntu-touch-android.sh
@@ -0,0 +1,33 @@
+!#/bin/sh
+
+set -e
+
+codename=$1
+builddir=$codename-build
+
+[ "$(dpkg --print-architecture)" = "amd64" ] || exit 1
+
+# set up multiarch
+dpkg --print-foreign-architectures | grep -q i386 || dpkg --add-architecture i386
+apt-get update
+
+# add cross build env including the needed i386 packages
+apt-get -y install git gnupg flex bison gperf build-essential \
+    zip bzr curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
+    libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386 \
+    libgl1-mesa-dev g++-multilib mingw32 tofrodos phablet-tools \
+    python-markdown libxml2-utils xsltproc zlib1g-dev:i386 schedtool \
+	openjdk-6-jdk
+
+# get the git tree
+phablet-dev-bootstrap -v $codename $builddir
+
+cd $builddir
+repo sync
+
+. build/envsetup.sh
+brunch $codename
+
+cd -
+cp $builddir/out/target/product/$codename/*-$codename.zip .
+rm -rf $buildir
